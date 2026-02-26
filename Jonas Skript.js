b41eeb9ff1d416adbc7d8761ce58126b923d7fbb
voxtec-com <einjonasmanagement@outlook.de>
2026-02-26 21:05:13 +0100

    Create Jonas Skript.js

---
+++ Jonas Skript.js
@@ -0,0 +1,8 @@
+function button1() {
+      document.getElementById("infoText").innerText =
+        "Aktuelle Infos kommen bald online!"; 
+  
+}function button2() {
+  
+  document.getElementById('twitchLink').innerHTML =
+    '<a href="https://www.twitch.tv/einjonass?lang=de" target="_blank">Hier klicken für Twitch</a>';}
